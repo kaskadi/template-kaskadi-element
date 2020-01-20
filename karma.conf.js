7a075989059cada293d87b497a82ad1f2faea9cf
Holger Will <h.will@klimapartner.de>
2020-01-20 08:18:37 +0100

    code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,21 @@
 module.exports = config => {
   config.set({
     frameworks: ['mocha', 'chai'],
-    files: [{ pattern: 'test/**/*.js', type: 'module' }, { pattern: '*.js', included: false }],
-    reporters: ['progress'],
+    files: [{ pattern: 'test/**/*.js', type: 'module' }, { pattern: '*.js', included: false, type: 'module' }],
+    preprocessors: {
+      '*.js': 'coverage'
+    },
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        { type: 'text' },
+        { type: 'lcovonly' }
+      ],
+      instrumenterOptions: {
+        istanbul: { esModules: true }
+      }
+    },
+    reporters: ['progress', 'coverage'],
     port: 9876, // karma web server port
     colors: true,
     browsers: ['ChromeHeadless'],
