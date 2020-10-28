1dbcab3aa5b142990138a9d1b85cabcd624ad073
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-28 13:22:07 +0100

    Test cleanup

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,27 +1,24 @@
 /* eslint-env browser, mocha */
 import '../template-kaskadi-element.js'
 describe('template-kaskadi-element', () => {
-  it('should have a black text color', async () => {
-    const elem = document.createElement('template-kaskadi-element')
+  var elem = document.createElement('template-kaskadi-element')
+  before(async () => {
     document.body.appendChild(elem)
     await elem.updateComplete
+  })
+  it('should have a black text color', async () => {
     const cs = getComputedStyle(elem.shadowRoot.querySelector('div'))
     cs.color.should.equal('rgb(0, 0, 0)')
   })
   it('should not render anything if no phrase is provided', async () => {
-    const elem = document.createElement('template-kaskadi-element')
-    document.body.appendChild(elem)
-    await elem.updateComplete
     elem.shadowRoot.querySelector('#en').textContent.should.equal('')
   })
   it('should render the sentences provided via the phrase property', async () => {
-    var elem = document.createElement('template-kaskadi-element')
     elem.setAttribute('phrase', JSON.stringify({
       en: 'Hello World!',
       de: 'Hallo Welt!',
       fr: 'Bonjour monde !'
     }))
-    document.body.appendChild(elem)
     await elem.updateComplete
     elem.shadowRoot.querySelector('#en').textContent.should.equal('Hello World!')
     elem.shadowRoot.querySelector('#de').textContent.should.equal('Hallo Welt!')
