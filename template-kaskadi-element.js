19ba3aeaef306c52a95bae272d02807b583463ad
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-28 12:20:06 +0100

    Fixed default + logic

--- template-kaskadi-element.js
+++ template-kaskadi-element.js
@@ -8,7 +8,7 @@ import { lang, translate, KaskadiElement, css, html } from 'https://cdn.klimapar
  *
  * @module template-kaskadi-element
  *
- * @param {string} [phrase={&quot;en&quot;:&quot;Hello World!&quot;,&quot;de&quot;:&quot;Hallo Welt!&quot;,&quot;fr&quot;:&quot;Bonjour monde !&quot;}] - set of localized phrases that can be used for localization via our `translate` and `lang` module. This is a stringified object containing language as key (`en`, `de`, etc.) and the corresponding phrase as value.
+ * @param {string} [phrase] - set of localized phrases that can be used for localization via our `translate` and `lang` module. This is a stringified object containing language as key (`en`, `de`, etc.) and the corresponding phrase as value.
  *
  * @example
  *
@@ -18,11 +18,7 @@ import { lang, translate, KaskadiElement, css, html } from 'https://cdn.klimapar
 class TemplateKaskadiElement extends KaskadiElement {
   constructor () {
     super()
-    this.phrase = {
-      en: 'Hello World!',
-      de: 'Hallo Welt!',
-      fr: 'Bonjour monde !'
-    }
+    this.phrase = ''
   }
 
   static get styles () {
@@ -30,7 +26,6 @@ class TemplateKaskadiElement extends KaskadiElement {
       :host{
         display: inline-block;
       }
-      div{color: red}
     `
   }
 
@@ -41,7 +36,7 @@ class TemplateKaskadiElement extends KaskadiElement {
   }
 
   render () {
-    const phrase = lang`${this.phrase}`
+    const phrase = lang`${JSON.parse(this.phrase)}`
     return html`
       <div id="en">${translate(phrase, 'en')}</div>
       <div id="de">${translate(phrase, 'de')}</div>
